a93a51b16b3f22e6f6f3164e669893ddc8c74236
moo-on <wns0159@gmail.com>
2021-03-07 23:45:20 +0900

    pybithumb_doc

---
+++ pybithumb_manual_basic.py
@@ -0,0 +1,19 @@
+import pybithumb
+import time
+
+tickers = pybithumb.get_tickers() #코인 정보
+
+
+price = pybithumb.get_current_price("BTC") #현재 가격 #ALL
+print(price)
+
+detail = pybithumb.get_market_detail("BTC") #저가 고가 거래금액 거래량
+
+orderbook = pybithumb.get_orderbook("BTC")
+# bids매수호가  ask매도호가
+
+bids = orderbook['bids'] # quantity, price
+
+all = pybithumb.get_current_price('ALL') # 211pg
+for k,v in all.items():
+    print(k, v['closing_price'])
